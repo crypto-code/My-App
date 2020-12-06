0e0f803578cbd88a684ba919f1fccf17e6cff663
Atin Sakkeer Hussain <atinsaki@Atins-MacBook-Pro.local>
2020-12-06 23:56:06 +0400

    Homepage Navbar done

--- app/javascript/components/Title.js
+++ app/javascript/components/Title.js
@@ -1,6 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import logo from "../../assets/images/logo2.png"
+import logo from "../../assets/images/logo.png"
 
 class Title extends React.Component {
     render () {
